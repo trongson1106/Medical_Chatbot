a2f8bcf447127bc424b8180b480bcaa2f26205f4
son1106 <son1106@students.ubc.ca>
2026-03-01 12:18:38 -0800

    project structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+# Createing directories
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directories and files created successfully"
